e441fc199dfc0ea2890b53d78ae47f549b34132c
JGabriel Gruber <jgabrielgruber@hotmail.com>
2019-05-20 15:03:39 -0300

    Criada páginas base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'signup', loadChildren: './signup/signup.module#SignupPageModule' },
+  { path: 'chamado', loadChildren: './chamado/chamado.module#ChamadoPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/chamado/chamado.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ChamadoPage } from './chamado.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ChamadoPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ChamadoPage]
+})
+export class ChamadoPageModule {}
