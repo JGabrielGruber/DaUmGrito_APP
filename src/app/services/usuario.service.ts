b2597da6d4f7521cc631ca18bd5137c4558ae31b
JGabriel Gruber <jgabrielgruber@hotmail.com>
2019-05-23 14:27:31 -0300

    Criado service para Usuário;

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,24 @@
+import { Http } from './../models/http';
+import { HttpService } from './http.service';
+import { Configs } from './../configs';
+import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario';
+import { Service } from '../base/Service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UsuarioService extends Service<Usuario>{
+
+	url: string = `${Configs.url}oauth`;
+
+	constructor(
+		public http: HttpService) {
+		super(`${Configs.url}oauth`, http);
+	}
+
+	async getData(token): Promise<Http> {
+		return this.http.get(`${this.url}/token`, token);
+	}
+
+}
