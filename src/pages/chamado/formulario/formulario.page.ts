d5c018572edea58422015fc23faeeade7102da23
JGabriel Gruber <jgabrielgruber@hotmail.com>
2019-05-25 17:59:06 -0300

    Implemenmtado o formulário de chamado;

--- src/pages/chamado/formulario/formulario.page.ts
+++ src/pages/chamado/formulario/formulario.page.ts
@@ -1,15 +1,25 @@
+import { Chamado } from './../../../app/models/chamado.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.page.html',
-  styleUrls: ['./formulario.page.scss'],
+	selector: 'app-formulario',
+	templateUrl: './formulario.page.html',
+	styleUrls: ['./formulario.page.scss'],
 })
 export class FormularioPage implements OnInit {
+	chamado: Chamado = new Chamado();
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	async takePicture(): Promise<void> {
+		this.chamado.foto = "https://i1.sndcdn.com/artworks-000203162222-lfsd7a-t500x500.jpg";
+	}
+
+	async submit(): Promise<void> {
+
+	}
 
 }
