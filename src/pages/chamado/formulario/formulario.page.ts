43de396829effb171da87b0b2c5546c19016e925
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-06-01 09:23:57 -0300

    Corrigida implementação da geolocalização;

--- src/pages/chamado/formulario/formulario.page.ts
+++ src/pages/chamado/formulario/formulario.page.ts
@@ -1,27 +1,46 @@
+import { LoginService } from './../../../app/services/login.service';
+import { ChamadoService } from './../../../app/services/chamado.service';
 import { Chamado } from './../../../app/models/chamado.model';
 import { Component, OnInit } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 import { CameraService } from './../../../app/services/camera.service';
+import { Platform } from '@ionic/angular';
+import { Location } from '@angular/common';
 
 @Component({
 	selector: 'app-formulario',
 	templateUrl: './formulario.page.html',
 	styleUrls: ['./formulario.page.scss'],
 })
-export class FormularioPage implements OnInit {
+export class FormularioPage implements OnInit{
 	chamado: Chamado = new Chamado();
 
 	constructor(
 		private geolocation: Geolocation,
-		private cameraService: CameraService
-	) { }
+		private cameraService: CameraService,
+		private platform: Platform,
+		private chamadoService: ChamadoService,
+		private loginService: LoginService,
+		private location: Location
+	) {}
 
 	ngOnInit() {
-		this.geolocation.getCurrentPosition().then((resp) => {
-			this.chamado.localizacao.latitude	= resp.coords.latitude;
-			this.chamado.localizacao.longitude	= resp.coords.longitude;
+		this.getLocation();
+	}
+
+	async getLocation() {
+		await this.platform.ready();
+		await this.geolocation.getCurrentPosition({
+			timeout: 30000,
+			enableHighAccuracy: true
+		}).then((resp) => {
+			this.chamado.localizacao	= {
+				latitude: resp.coords.latitude,
+				longitude: resp.coords.longitude
+			};
 		}).catch((error) => {
 			alert("Problema ao obter sua localização");
+			console.log(error);
 		});
 	}
 
@@ -32,7 +51,11 @@ export class FormularioPage implements OnInit {
 	}
 
 	async submit(): Promise<void> {
-
+		await this.getLocation();
+		console.log(this.chamado);
+		if ((await this.chamadoService.post(this.chamado, (await this.loginService.getToken()))).success) {
+			this.location.back();
+		}
 	}
 
 }
