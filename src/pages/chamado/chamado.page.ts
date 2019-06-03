fadbf27c3d1a8d13a7fecce5c8640a990bfd9095
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2019-06-03 16:45:55 -0300

    Implementado lista de chamados do usuário;

--- src/pages/chamado/chamado.page.ts
+++ src/pages/chamado/chamado.page.ts
@@ -1,15 +1,47 @@
+import { LoginService } from './../../app/services/login.service';
+import { ClienteReducer } from './../../app/models/clienteR.model';
+import { ChamadoService } from './../../app/services/chamado.service';
 import { Component, OnInit } from '@angular/core';
+import { Chamado } from 'src/app/models/chamado.model';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+interface AppState {
+	usuario: ClienteReducer
+}
 
 @Component({
-  selector: 'app-chamado',
-  templateUrl: './chamado.page.html',
-  styleUrls: ['./chamado.page.scss'],
+	selector: 'app-chamado',
+	templateUrl: './chamado.page.html',
+	styleUrls: ['./chamado.page.scss'],
 })
 export class ChamadoPage implements OnInit {
+	lista: Array<Chamado>	= new Array<Chamado>();
+	usuario$: Observable<ClienteReducer>;
 
-  constructor() { }
+	constructor(
+		private chamadoService: ChamadoService,
+		private loginService: LoginService,
+		private store: Store<AppState>
+	) {
+		this.usuario$	= this.store.select('cliente');
+		this.listar();
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	async listar(): Promise<void> {
+		let token	= await this.loginService.getToken();
+		this.usuario$.subscribe(async (data) => {
+			if (data.data.cpf) {
+				let response	= await this.chamadoService.getByCliente(data.data, token);
+				if (response && response.success) {
+					this.lista	= response.data;
+				}
+			}
+		})
+		
+	}
 
 }
